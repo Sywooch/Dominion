830759b6b4f82c0892caebdfc1d6158be31ceb0e
ruslan <ruslan@2a8b18c9-bd72-244c-9897-a74ca54ae12b>
2013-04-19 14:45:22 +0000

    remove windows style crlf
    git-svn-id: http://svn.dev/svn/SVNRepository/dominion.ua/src/trunk@7153 2a8b18c9-bd72-244c-9897-a74ca54ae12b

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,57 +1,38 @@
 <?php
+/**
+ * Base bootstrap
+ */
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
-
-    public function __construct($application) {
+    public function __construct($application)
+    {
         parent::__construct($application);
     }
 
-    public function run() {
+    public function run()
+    {
         parent::run();
     }
 
-    protected function _initAutoload() {
-        $loader = new Zend_Application_Module_Autoloader(array(
-                    'namespace' => '',
-                    'basePath' => APPLICATION_PATH));
-
-        return $loader;
-    }
-
-    protected function _initSession() {
-        // should probably only do this for modules other than API? -- GAW
-        $session = new Zend_Session_Namespace('frontenf', true);
-
-        return $session;
-    }
-
-    protected function _initConfig() {
-        $config = new Zend_Config($this->getOptions());
-        Zend_Registry::set('config', $config);
-        return $config;
-    }
-
-    protected function _initConfiguration() {
-        $options = $this->getApplication()->getOptions();
-
-        return $options;
-    }
-
-    public function _initHelper() {
+    public function _initHelper()
+    {
         Zend_Controller_Action_HelperBroker::addHelper(
-                new App_Model_ModelLoader()
+            new App_Model_ModelLoader()
         );
 
         Zend_Controller_Action_HelperBroker::addHelper(
-                new App_Controller_Helper_HelperLoader()
+            new App_Controller_Helper_HelperLoader()
         );
     }
 
     /**
      * Bootsturp routing for ЧПУ URL
-     * @return type 
+     *
+     * @return type
      */
-    public function _initRouter() {
+    public function _initRouter()
+    {
         $front = Zend_Controller_Front::getInstance();
         $rout = new App_Controller_Router_Bootstrap($front);
         $rout->setRouting();
@@ -59,7 +40,41 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
         return $rout->getRouter();
     }
 
-    protected function _initView() {
+    protected function _initAutoload()
+    {
+        $loader = new Zend_Application_Module_Autoloader(array(
+            'namespace' => '',
+            'basePath' => APPLICATION_PATH
+        ));
+
+        return $loader;
+    }
+
+    protected function _initSession()
+    {
+        // should probably only do this for modules other than API? -- GAW
+        $session = new Zend_Session_Namespace('frontenf', true);
+
+        return $session;
+    }
+
+    protected function _initConfig()
+    {
+        $config = new Zend_Config($this->getOptions());
+        Zend_Registry::set('config', $config);
+
+        return $config;
+    }
+
+    protected function _initConfiguration()
+    {
+        $options = $this->getApplication()->getOptions();
+
+        return $options;
+    }
+
+    protected function _initView()
+    {
         $view = new App_View_Xslt;
 
         $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
