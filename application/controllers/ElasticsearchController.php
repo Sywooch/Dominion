5d0f6ec1966e77331ae06b937334de8f5c29fce2
kos21 <kosman@email.ua>
2013-06-27 23:12:55 +0400

    Добавление файла
    Signed-off-by: kos21 <kosman@email.ua>

---
+++ application/controllers/ElasticsearchController.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Константин
+ * Date: 30.05.13
+ * Time: 16:15
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * Controller for execute Elastic Search
+ *
+ * Class ElasticSearchController
+ */
+class ElasticsearchController extends App_Controller_Frontend_Action
+{
+    /**
+     * Config parameters
+     *
+     * @var objects
+     */
+    private $config;
+
+    /**
+     * Initialize system controller for get request
+     */
+    public function init()
+    {
+        parent::init();
+
+        $this->config = Zend_Registry::get("config");
+
+        if (empty($this->config)) {
+            throw new Exception("Error: configuration is not include class: " . __CLASS__ . ", line: " . __LINE__);
+        }
+    }
+
+    /**
+     * Action for execute elastic search
+     */
+    public function indexAction()
+    {
+        $term = $this->_getParam("term");
+
+        if (empty($term)) {
+            $this->_helper->json($term);
+        }
+        $parameters = $this->config->toArray();
+
+        /** @var $helpersElasticExecute Helpers_ExecuteElastic */
+        $helpersElasticExecute = $this->_helper->helperLoader("ExecuteElastic");
+        $helpersElasticExecute->setParameters($parameters['search_engine']);
+        $results = $helpersElasticExecute->runElasticGET($term);
+
+        if (empty($results)) {
+            $this->_helper->json($results);
+        }
+
+        $executeElastic = $this->_helper->helperLoader("ExecuteElastic");
+
+        $results = $executeElastic->executeFormatData(
+            $results, $this->currency,
+            $this->_helper->helperLoader("Prices_Recount"),
+            $this->_helper->helperLoader("Prices_Discount"),
+            true);
+
+        $results[count($results)]["else_results"] = $term;
+
+        $this->_helper->json($results);
+    }
+}
