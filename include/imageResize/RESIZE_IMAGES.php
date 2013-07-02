a15c23cfad769379f03ad6107f14d671721434c3
Ruslan <rbocharov@gmail.com>
2013-07-03 01:11:05 +0300

    set new composer libs

--- include/imageResize/RESIZE_IMAGES.php
+++ include/imageResize/RESIZE_IMAGES.php
@@ -1,8 +1,33 @@
 <?php
-require_once 'imageResizeLib/config_mage.ini.php';
+
+require_once realpath(dirname(__FILE__) . '../../../application/configs/') . '/config.php';
+use Imagine\Image\Point;
+
+//require_once 'imageResizeLib/config_mage.ini.php';
 //ini_set('upload_max_filesize','10M');
 
-require ('core.php');
+//require ('core.php');
+
+
+
+
+use Imagine\Image\Box;
+use Imagine\Image\ImageInterface;
+use Imagine\GD\Imagine;
+
+$imagine = new Imagine();
+
+$imageTest = $imagine->open('d:\Projects\756000.com.ua\src\44307.jpg');
+
+$box = $imageTest->getSize();
+
+$box1 = $box->widen(100);
+$box1 = $box->heighten(300);
+
+$imageTest->resize($box1)
+    ->save('d:\Projects\756000.com.ua\src\animated-resized.jpg', array('flatten' => true));
+
+
 require 'imageResizeLib/makeImageToStandart.php';
 $resize = new Resize();
 $resize->addImage();
