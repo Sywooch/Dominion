3234d8bbdc145134090211cf91df195bfe6b1d04
Ruslan <rbocharov@gmail.com>
2013-07-04 01:15:00 +0300

    test Resize

--- include/imageResize/RESIZE_IMAGES.php
+++ include/imageResize/RESIZE_IMAGES.php
@@ -1,23 +1,35 @@
 <?php
 
-require_once realpath(dirname(__FILE__) . '../../../application/configs/') . '/config.php';
-use Imagine\Image\Point;
+require_once __DIR__ . '../../../application/configs/config.php';
 
-//require_once 'imageResizeLib/config_mage.ini.php';
-//ini_set('upload_max_filesize','10M');
+use Symfony\Component\ClassLoader\UniversalClassLoader;
 
-//require ('core.php');
+$loader = new UniversalClassLoader();
+
+$loader->registerPrefixes(
+    array(
+        'ContextSearch_' => LIBRARY_PATH,
+        "Helpers_" => APPLICATION_PATH,
+        "models_" => APPLICATION_PATH,
+        "Format_" => LIBRARY_PATH,
+        "ImageResize_" => LIBRARY_PATH
+    )
+);
+$loader->register();
+$config = new Zend_Config_Ini(APPLICATION_PATH . "/configs/application.ini", "production");
+Zend_Registry::set("config", $config);
 
 
 
+$resizer = new ImageResize_Resize(100,200, 20);
 
-use Imagine\Image\Box;
-use Imagine\Image\ImageInterface;
-use Imagine\GD\Imagine;
+$resizer->resize('d:\Projects\756000.com.ua\src\44307.jpg', 'd:\Projects\756000.com.ua\src\animated-resized-notflatten.jpg');
+
+die();
 
 $imagine = new Imagine();
 
-$imageTest = $imagine->open('d:\Projects\756000.com.ua\src\44307.jpg');
+$imageTest = $imagine->open('d:\Projects\756000.com.ua\src\44307111.jpg');
 
 $box = $imageTest->getSize();
 
@@ -25,7 +37,7 @@ $box1 = $box->widen(100);
 $box1 = $box->heighten(300);
 
 $imageTest->resize($box1)
-    ->save('d:\Projects\756000.com.ua\src\animated-resized.jpg', array('flatten' => true));
+    ->save('d:\Projects\756000.com.ua\src\animated-resized-notflatten.jpg', array('flatten' => true));
 
 
 require 'imageResizeLib/makeImageToStandart.php';
