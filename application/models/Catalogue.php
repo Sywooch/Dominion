424c0bf355bed399e221bd35ab37d8efb21d8c77
kostya <galancevkonstantin21@gmail.com>
2014-08-20 20:17:57 +0300

    Bug fix with get column name

--- application/models/Catalogue.php
+++ application/models/Catalogue.php
@@ -445,11 +445,12 @@ class models_Catalogue extends ZendDBEntity
      */
     public function getCatalogsIncludeBrandsList($catalogId = null)
     {
-
         $sql = "SELECT
                   C.NAME,
                   C.IMAGE1,
                   C.REALCATNAME,
+                  C.CATALOGUE_ID,
+                  C.PARENT_ID
                   GROUP_CONCAT(DISTINCT CONCAT(B.NAME, '#', B.ALT_NAME) ORDER BY B.NAME) AS BRANDS
                 FROM CATALOGUE C
                   JOIN CATALOGUE_BRAND_VIEW CBV1 USING (CATALOGUE_ID)
