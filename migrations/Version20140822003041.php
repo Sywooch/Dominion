0aa87c36e3a505de15e96d16a80199c1e11ba37c
Ruslan <rbocharov@gmail.com>
2014-08-25 14:59:13 +0300

    bugFix: not correct NULL parameter

--- migrations/Version20140822003041.php
+++ migrations/Version20140822003041.php
@@ -3,7 +3,7 @@
 namespace DominionMigrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration,
-    Doctrine\DBAL\Schema\Schema;
+  Doctrine\DBAL\Schema\Schema;
 use Doctrine\DBAL\Types\Type;
 
 class Version20140822003041 extends AbstractMigration
@@ -11,10 +11,11 @@ class Version20140822003041 extends AbstractMigration
     public function up(Schema $schema)
     {
         $schema->getTable("CATALOGUE")->addColumn(
-            "IMAGE_MENU",
-            "string"
-        )->setDefault("NULL")
-            ->setLength(50);
+          "IMAGE_MENU",
+          "string"
+        )->setDefault(null)
+          ->setNotnull(false)
+          ->setLength(50);
     }
 
     public function down(Schema $schema)
